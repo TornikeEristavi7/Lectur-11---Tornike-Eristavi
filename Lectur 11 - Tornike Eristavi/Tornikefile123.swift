3a2cdb9902209ad29d5bc8a9b83d8a2b9ec1cc1f
Tornikeeristavi7 <tornikeeristavi43@gmail.com>
2024-04-30 12:19:27 +0400

    Cristiano Ronaldo siuuuu

---
+++ Lectur 11 - Tornike Eristavi/Tornikefile123.swift
@@ -0,0 +1,11 @@
+//
+//  Tornikefile123.swift
+//  Lectur 11 - Tornike Eristavi
+//
+//  Created by Tornike Eristavi on 30.04.24.
+//
+
+import Foundation
+
+
+let bestFootballerEver = "Cristiano Ronaldo"
